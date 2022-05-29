722106726be8f5932af73686bca868c18da2c934
BarKabal <bkabala00@gmail.com>
2022-05-29 17:38:25 +0200

    Dodałem wypolerowane materiały

--- GK3/src/main/java/Material.java
+++ GK3/src/main/java/Material.java
@@ -12,8 +12,11 @@ public class Material {
     public static ArrayList<Material> materials = new ArrayList<>() {
         {
             add(new Material(0.2125, 0.714, 0.3935, 25.6, "Bronze", new Color(205, 127, 50)));
+            add(new Material(0.25, 0.4, 0.774597, 76.8, "Polished Bronze", new Color(205, 127, 50)));
             add(new Material(0.19225, 0.50754, 0.508273, 51.2, "Silver", new Color(192,192,192)));
+            add(new Material(0.23125, 0.2775, 0.773911, 89.6, "Polished Silver", new Color(192,192,192)));
             add(new Material(0.24725, 0.75164, 0.628281, 51.2, "Gold", new Color(255,215,0)));
+            add(new Material(0.24725, 0.34615, 0.797357, 83.2, "Polished Gold", new Color(255,215,0)));
             add(new Material(0.329412, 0.780392, 0.992157, 27.8974, "Brass", new Color(173, 153, 12)));
             add(new Material(0.25, 0.4, 0.774597, 76.8, "Chrome", new Color(219,226,233)));
             add(new Material(0.105882, 0.427451, 0.333333, 9.84615, "Pewter", new Color(142,146,148)));
